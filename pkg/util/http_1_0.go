9a874d8fcb8adfce7fc991ff7f75b70452aa1649
Derek Argueta <deargueta@tesla.com>
2021-06-14 12:55:48 -0700

    add a note

--- pkg/util/http_1_0.go
+++ pkg/util/http_1_0.go
@@ -5,6 +5,8 @@ import (
 	"os/exec"
 )
 
+// Seems that the Go standard library does not support HTTP/1.0 requests?
+// https://github.com/golang/go/blob/d4f34f8c63b753160716e9f90ca530016ce019d7/src/net/http/transport.go#L74-L78
 func Http10Request(url string) bool {
 	out, err := exec.Command("curl", "-svo", "/dev/null", "--http1.0", url).CombinedOutput()
 	if err != nil {
