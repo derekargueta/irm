7f2ca5172dded0a5fcf7514f5ec072ee6c3f02a1
Derek Argueta <deargueta@tesla.com>
2021-07-17 20:24:20 -0700

    start http server

---
+++ cmd/server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type CommandArgs struct {
+	port int
+	webDir string
+}
+
+func parseArgs() *CommandArgs {
+	var port int
+	var webDir string
+	flag.IntVar(&port, "port", 8080, "TCP port that the HTTP server will listen on.")
+	flag.StringVar(&webDir, "web-dir", "web", "The directory from which files are served over HTTP.")
+	flag.Parse()
+	return &CommandArgs{
+		port: port,
+		webDir: webDir,
+	}
+}
+
+type handler struct {
+	content string
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	io.WriteString(w, h.content)
+}
+
+func main() {
+	args := parseArgs()
+
+	// Read the index.html into memory.
+	dat, err := ioutil.ReadFile(args.webDir + "/index.html")
+	if err != nil {
+		panic(err)
+	}
+	content := string(dat)
+
+	// Create a handler that will serve the file.
+	handler := &handler{content: content}
+	http.Handle("/", handler)
+
+	// Bind the TCP listener.
+	listenAddr := fmt.Sprintf(":%d", args.port)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
