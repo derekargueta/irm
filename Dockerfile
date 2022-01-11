1ef3131646cda86351ac783081a596bcf6585279
Henry Palma <tavovnt@gmail.com>
2022-01-11 11:16:33 -0600

    stackpath complete

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,5 @@ ADD . .
 RUN go get -d -v ./...
 ENTRYPOINT go run ./cmd/analyze -f="./domains/testset.txt" -w="30" -git=0
 
-# url: go run ./cmd/analyze -w="30" -git=0
-# file: go run ./cmd/analyze -f="./domain 
-# go run ./cmd/analyze -f="./domains/testset.txt" -w="30" -git=0
+# url: go run ./cmd/analyze -url="" -w="30" -git=0
+# file: go run ./cmd/analyze -f="./domains/testset.txt" -w="30" -git=0
--- pkg/irm/probes/max_CDN.go
+++ pkg/irm/probes/max_CDN.go
@@ -12,8 +12,7 @@ type MaxCDN struct {
 }
 
 /*
-Doesnt work atm, supported Max CDN websites
-dont return true when ran through cidr list
+Maxcdn is now stackpath
 
 */
 
@@ -26,6 +25,7 @@ func (h *MaxCDN) Run(domain string) *ProbeResult {
 	enabledtotal := false
 	_, httperr := http.NewRequest("GET", domain, nil)
 	if httperr != nil {
+		fmt.Println("http error on maxcdn")
 		return &ProbeResult{
 
 			Supported: false,
@@ -37,12 +37,29 @@ func (h *MaxCDN) Run(domain string) *ProbeResult {
 	for _, x := range ips {
 
 		if x.To4() != nil {
-			for _, cidr := range h.Ipv4_cidr {
-				if cidr.Contains(x) {
 
-					enabledtotal = true
-					fmt.Println("enabled true on maxcdn")
+			for _, cidr := range h.Ipv4_cidr {
+				if cidr.String() == "151.139.0.0/17" { //for some reason, this stackpath ip doesn't return it's organization when testing with stackpath websites
+					_, temp, err := net.ParseCIDR("151.139.0.0/16") //however, changing the port to this returns it
+					if err != nil {
+						fmt.Println("error on maxcdn on 151.139.0.0/16")
+					}
+					if temp.Contains(x) {
+						enabledtotal = true
+						fmt.Println("FOUND TRUE ON MAXCDN")
+						break
+					}
 				}
+
+				if cidr.Contains(x) {
+					enabledtotal = true
+					fmt.Println("FOUND TRUE ON MAXCDN")
+					break
+				}
+
+			}
+			if enabledtotal == true {
+				break
 			}
 
 		}
