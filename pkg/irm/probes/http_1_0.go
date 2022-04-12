f2704f9bed4672e766a545e26e0de579be837d65
tavofn <tavovnt@gmail.com>
2022-04-12 04:52:29 +0000

    http1.0 fixes

--- pkg/irm/probes/http_1_0.go
+++ pkg/irm/probes/http_1_0.go
@@ -1,6 +1,8 @@
 package probes
 
 import (
+	"context"
+	"time"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -9,29 +11,42 @@ import (
 type Http10probe struct{}
 
 func (h *Http10probe) Run(domain string) *ProbeResult {
-	out, err := exec.Command("curl", "-svo", "/dev/null", "--no-keepalive", "--http1.0", domain).CombinedOutput()
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	out, err := exec.CommandContext(ctx, "curl", "-svo", "/dev/null", "--no-keepalive", "--http1.0", domain).CombinedOutput()
 	enabled := false
 	if err != nil {
 		fmt.Println("error in http1.0 " + err.Error())
 
+	} else if ctx.Err() == context.DeadlineExceeded {
+		fmt.Println("Timeout http1.0 on" + domain)
 	} else {
 		if strings.Contains(string(out), "HTTP/1.0 200 OK") {
 			enabled = true
 
 		} else {
-			out2, err := exec.Command("curl", "-svo", "/dev/null", "--no-keepalive", "--http1.0", "https://"+domain).CombinedOutput()
+			out2, err := exec.CommandContext(ctx,"curl", "-svo", "/dev/null", "--no-keepalive", "--http1.0", "https://"+domain).CombinedOutput()
 
 			if err != nil {
 				fmt.Println("error in http1.0 " + err.Error())
 
-			}
+			} else if ctx.Err() == context.DeadlineExceeded {
 
-			if strings.Contains(string(out2), "HTTP/1.0 200 OK") {
-				enabled = true
+				fmt.Println("timeout http1.0 on https" + domain)
+			} else {
+
+				if strings.Contains(string(out2), "HTTP/1.0 200 OK") {
+					enabled = true
 
 			}
 		}
 	}
+}
+
+
+
+
 
 	return &ProbeResult{
 		Supported: enabled,
