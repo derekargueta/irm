d8e75f0fd473a581442f5d72b3203262e60d80d7
Henry Palma <tavovnt@gmail.com>
2022-04-29 13:11:21 -0500

    singledomainweb

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"text/template"
+
+	"github.com/derekargueta/irm/pkg/irm/probes"
 )
 
 type CommandArgs struct {
@@ -28,14 +31,53 @@ func parseArgs() *CommandArgs {
 type handler struct {
 	content string
 }
+type myvals struct {
+	Http10 string
+	Http11 string
+	Http12 string
+	Http13 string
+}
 
-func (h *handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
-	io.WriteString(w, h.content)
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	t, err := template.ParseFiles("./web/index.html")
+	if err != nil {
+		fmt.Print("iuykfsduyfsdf")
+	}
+	t.Execute(w, nil)
+}
+
+func singledomain(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("./web/index.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		http10 := (&probes.Http10probe{}).Run(r.Form.Get("website"))
+		http1Result := (&probes.HTTP11Probe{}).Run(r.Form.Get("website"))
+		http2Result := (&probes.HTTP2Probe{}).Run(r.Form.Get("website"))
+		http3Result := (&probes.HTTP3Probe{}).Run(r.Form.Get("website"))
+
+		// logic part of log in
+
+		test := myvals{
+			Http10: strconv.FormatBool(http10.Supported),
+			Http11: strconv.FormatBool(http1Result.Supported),
+			Http12: strconv.FormatBool(http2Result.Supported),
+			Http13: strconv.FormatBool(http3Result.Supported),
+		}
+		t, _ := template.ParseFiles("./web/index.html")
+		t.Execute(w, test)
+		//	fmt.Println("website:", r.Form["website"])
+
+	}
 }
 
 func main() {
 	args := parseArgs()
-
 	//Read the index.html into memory.
 	// dat, err := ioutil.ReadFile(args.webDir + "/index.html")
 	// if err != nil {
@@ -50,8 +92,15 @@ func main() {
 	// http.Handle("/", http.FileServer(http.Dir(args.webDir)))
 	// http.Handle("/js", http.FileServer(http.Dir(args.webDir+"/js")))
 
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(args.webDir))))
-	// Bind the TCP listener.
+	http.HandleFunc("/", ServeHTTP)
+	http.HandleFunc("/singledomain", singledomain)
+	http.HandleFunc("/about.html", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/about.html")
+	})
+	//http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(args.webDir))))
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./web/css"))))
+
 	listenAddr := fmt.Sprintf(":%d", args.port)
 	log.Fatal(http.ListenAndServe("127.0.0.1"+listenAddr, nil))
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.15
 require (
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/likexian/whois v1.12.4
-	github.com/lucas-clemente/quic-go v0.24.0
+	github.com/lucas-clemente/quic-go v0.27.0
+	//github.com/lucas-clemente/quic-go v0.24.0
 	github.com/miekg/dns v1.1.43
 	// github.com/go-gota/gota v0.10.1 // indirect
 	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
